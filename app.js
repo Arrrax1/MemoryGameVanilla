9e2902ae3beb652473f412def2e2148910cb72b2
Arrrax1 <bimobimo134@gmail.com>
2023-07-14 18:00:43 +0100

    logic finished

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// array of src
-let srcArray=['bolt','bomb','coin','heart','jewel','potion','shield','sword']
+// array of src //two of each element
+let srcArray=['bolt','bomb','coin','heart','jewel','potion','shield','sword','bolt','bomb','coin','heart','jewel','potion','shield','sword']
 
 // init board grid
 function initBoard() {
     let tempSrc = srcArray // use splice
-    for (let index = 0; index < 20; index++) {
+    for (let index = 0; index < 16; index++) {
         let image_index = Math.floor(Math.random()*(tempSrc.length-1))
         // card container
         let card = document.createElement('div')
@@ -25,18 +25,37 @@ function initBoard() {
         card.append(card_back)
         card.append(card_front)
         card.id = 'index' + index
-        //
+        // remove inserted card img
+        tempSrc.splice(image_index,1)
         document.querySelector('.board').append(card)
     }
 }
 initBoard()
 
+// card flip logic
+let card1=''
+let card2=''
 // flip test
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('click',()=>{
-        card.classList.add('flip')
-        setTimeout(() => {
-            card.classList.remove('flip')
-        }, 1000);
+        if (card1=='' && !card.classList.contains('flip')) { // check card is not already flipped, it causes reflip of already matched cards
+            card1=card
+            card.classList.add('flip')
+        } else if(card1!=card && card2=='' && !card.classList.contains('flip')){ // check card is not already flipped
+            card2=card
+            card.classList.add('flip')
+            if (card1.lastChild.firstChild.src==card2.lastChild.firstChild.src) {
+                console.log('match')
+                card1=''
+                card2=''
+            }else{
+                setTimeout(() => {
+                    card1.classList.remove('flip')
+                    card2.classList.remove('flip')
+                    card1=''
+                    card2=''
+                }, 1000);
+            }
+        }
     })
 });
